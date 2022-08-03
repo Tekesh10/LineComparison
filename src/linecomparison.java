991c67d1771a953516c725515343ce92d5263947
Tekesh10 <gtcsantekesh@gmail.com>
2022-08-03 23:07:27 +0530

    Length of a Line

--- src/linecomparison.java
+++ src/linecomparison.java
@@ -17,23 +17,6 @@ public class linecomparison {
         int y2 = sc.nextInt();
 
         double lineLengthXY = Math.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
-        System.out.println("Length of the line AB is => "+lineLengthXY);
-
-        System.out.println("Enter the value of line AB");
-        System.out.println("x1 coordinate");
-        int a1 = sc.nextInt();
-        System.out.println("y1 coordinate");
-        int b1 = sc.nextInt();
-        System.out.println("x2 coordinate");
-        int a2 = sc.nextInt();
-        System.out.println("y2 coordinate");
-        int b2 = sc.nextInt();
-
-        double lineLengthAB = Math.sqrt((a2 - a1)*(a2 - a1) + (b2 - b1)*(b2 - b1));
-
-        if (lineLengthXY == lineLengthAB)
-            System.out.println("Both lines are equal");
-        else
-            System.out.println("Both lines are not equal");
+        System.out.println("Length of the line XY is => "+lineLengthXY);
     }
 }
