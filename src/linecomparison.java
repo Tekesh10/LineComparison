dce60897debfb7150557d871c159a24f615691f7
Tekesh10 <gtcsantekesh@gmail.com>
2022-08-03 21:42:08 +0530

    Welcome Note

--- src/linecomparison.java
+++ src/linecomparison.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class linecomparison {
+public class linecomparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
 }
